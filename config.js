d75b13ac0b63d9d797c81ffbc508a86fdabfa26f
ClementGaill <cgaillard56390@gmail.com>
2026-01-19 12:04:10 +0100

    add public key

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// CE FICHIER DOIT ETRE DANS TON .GITIGNORE
+window.config = {
+    SUPABASE_URL: "https://bofuwdgprigtucyaawcq.supabase.co",
+    SUPABASE_ANON_KEY: "sb_publishable_T09vHKFa8fnGOJuc7oQnoQ_aGEMnSnR"
+};
